c2e74dbd3731d635ab00319228a2fead1470b508
jeyabalaji <jeyabalaji@fundscorner.com>
2019-09-13 17:02:36 +0530

    serial approach tested with the hardest problem

--- sudokusolve.go
+++ sudokusolve.go
@@ -31,9 +31,7 @@ func solve(sudokuIn sudoku, iter ...int) (sudokuOut sudoku, solved bool, iterati
 
 	*/
 
-	sudokuOut = make(sudoku, len(sudokuIn))
-	copy(sudokuOut, sudokuIn)
-
+	sudokuOut = sudokuIn.copy()
 	// mapResults := make([]cell, 0)
 	unfilledCount := 0
 
@@ -158,10 +156,7 @@ func solve(sudokuIn sudoku, iter ...int) (sudokuOut sudoku, solved bool, iterati
 
 func _solveWrapper(sudokuIn sudoku, iter int, cE cell, mutatedValue int, wg *sync.WaitGroup, c *chan sudokuChannel) {
 	defer wg.Done()
-	sudokuOut := make(sudoku, len(sudokuIn))
-	copy(sudokuOut, sudokuIn)
-
-	sudokuInter, _solved, _iteration, _err := solve(sudokuOut, iter)
+	sudokuInter, _solved, _iteration, _err := solve(sudokuIn, iter)
 	*c <- sudokuChannel{intermediate: sudokuInter, solved: _solved, iteration: _iteration, err: _err, cellMutated: cE, valueOption: mutatedValue}
 	log.Println("sent solution")
 }
